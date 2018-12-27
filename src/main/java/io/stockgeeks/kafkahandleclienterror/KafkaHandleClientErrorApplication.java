e3ed0e0954d35bb8a3b01a0e0c5bf750c5fe7a89
Marcos Maia <marcos.pais.alves.maia@ing.nl>
2018-12-27 11:04:43 +0100

    initial commit of the project

---
+++ src/main/java/io/stockgeeks/kafkahandleclienterror/KafkaHandleClientErrorApplication.java
@@ -0,0 +1,14 @@
+package io.stockgeeks.kafkahandleclienterror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaHandleClientErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaHandleClientErrorApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/io/stockgeeks/kafkahandleclienterror/KafkaHandleClientErrorApplicationTests.java
@@ -0,0 +1,17 @@
+package io.stockgeeks.kafkahandleclienterror;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaHandleClientErrorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
