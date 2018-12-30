928f45bd49d14679947e989aca2cf4fa564f142c
Marcos Maia <maia.marcos@gmail.com>
2018-12-30 09:38:32 +0100

    renamed classes added new module for pure kafka client

--- avro-clients/src/main/java/io/stockgeeks/kafkahandleclienterror/AvroClientApplication.java
+++ avro-clients/src/main/java/io/stockgeeks/kafkahandleclienterror/AvroClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaHandleClientErrorApplication {
+public class AvroClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KafkaHandleClientErrorApplication.class, args);
+		SpringApplication.run(AvroClientApplication.class, args);
 	}
 
 }
--- avro-clients/src/test/java/io/stockgeeks/kafkahandleclienterror/AvroClientApplicationTests.java
+++ avro-clients/src/test/java/io/stockgeeks/kafkahandleclienterror/AvroClientApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class KafkaHandleClientErrorApplicationTests {
+public class AvroClientApplicationTests {
 
 	@Test
 	public void contextLoads() {
